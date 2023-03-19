62a529931021e7c298e3b23181f57206af2d5286
wabarahona <89177976+wabarahona@users.noreply.github.com>
2023-03-18 21:41:37 -0600

    Ver.1.0 17MARZ Ver.1.1 18MARZ

--- app/src/main/java/com/example/tl01e1_027/directorio.java
+++ app/src/main/java/com/example/tl01e1_027/directorio.java
@@ -1,17 +1,26 @@
 package com.example.tl01e1_027;
 
 import android.app.AlertDialog;
+import android.content.Context;
 import android.content.DialogInterface;
+import android.content.Intent;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.BaseAdapter;
 import android.widget.Button;
 import android.widget.ListView;
+import android.widget.SearchView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
+
 
 import com.example.tl01e1_027.Tablas.Personas;
 import com.example.tl01e1_027.configuraciones.SQLiteconexion;
@@ -21,16 +30,16 @@ import java.util.ArrayList;
 
 public class directorio extends AppCompatActivity {
     SQLiteconexion conexion;
+    SearchView searchbar;
     ListView listView;
-    ArrayList<Personas> lista;
+    ArrayList<Personas> lista, list;
     ArrayList<String> ArregloPersonas;
-    Button buttonBack;
-
-
+    Button buttonBack,buttonCompartir,buttonViewImage,buttonEliminar,buttonActualizar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_directorio);
+
 
         conexion = new SQLiteconexion(this, Transacciones.NameDatabase, null, 1);
         listView = (ListView)findViewById(R.id.listPersonas);
@@ -38,6 +47,23 @@ public class directorio extends AppCompatActivity {
 
         ArrayAdapter adp = new ArrayAdapter(this, android.R.layout.simple_list_item_1,ArregloPersonas);
         listView.setAdapter(adp);
+        searchbar=(SearchView) findViewById(R.id.search_bar);
+        searchbar.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+
+            @Override
+            public boolean onQueryTextSubmit(String s) {
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String s) {
+
+                adp.getFilter().filter(s);
+
+                return false;
+            }
+        });
+
 
         listView.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
             @Override
@@ -63,11 +89,41 @@ public class directorio extends AppCompatActivity {
 
         buttonBack = (Button) findViewById(R.id.btnBack);
         buttonBack.setOnClickListener(view -> onBackPressed());
+        buttonCompartir =(Button)findViewById(R.id.btnCompartir);
+        buttonCompartir.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+        buttonViewImage=(Button)findViewById(R.id.btnVerImagen);
+        buttonEliminar=(Button)findViewById(R.id.btnEliminar);
+        buttonEliminar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        buttonActualizar=(Button)findViewById(R.id.btnActualizar);
+
+        listView.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+            @Override
+            public boolean onItemLongClick(AdapterView<?> parent, View view, int position, long id) {
+
+               lista.get(position);
+               Integer contactnumber;
+               contactnumber = lista.get(position).getTelefono();
+                Intent intent = new Intent(Intent.ACTION_DIAL, Uri.parse("tel: "+ contactnumber));
+                startActivity(intent);
+                return false;
+            }
+        });
 
 
-        }
+      }
 
-       private void ObtenerListaPersonas() {
+      private void ObtenerListaPersonas() {
         SQLiteDatabase db = conexion.getReadableDatabase();
         Personas persona = null;
         lista = new ArrayList<Personas>();
@@ -98,4 +154,5 @@ public class directorio extends AppCompatActivity {
 
 
     }
+
 }
